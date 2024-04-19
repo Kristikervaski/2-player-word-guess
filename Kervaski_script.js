d2f004bf531dadd42641fc46e5ff3479c4d89c32
Kistikervaski <kristikervaski@gmail.com>
2024-04-18 19:08:53 -0500

    adding cookie logic

--- Kervaski_script.js
+++ Kervaski_script.js
@@ -10,12 +10,40 @@ let button= document.getElementById("startGame");
 button.addEventListener("click",loadGameVariables);
 // Greet player one
 function loadGameVariables(){
-let playerOne= document.getElementById("nameOne").value;
-let secretWord=document.getElementById("secretWord");
-let button=document.getElementById("numberGuesses").value;
-let greeting=document.getElementById("greeting");
-greeting.innerHTML="Fantastic word choice, " + playerOne + " You have given " + numberGuesses.value + " guesses.";
-window.open("playerTwo.html")
-gamePlay();
-}
+    let form=document.getElementById("form");
+    let playerOne= document.getElementById("nameOne").value;
+    let secretWord=document.getElementById("secretWord").value;
+    console.log(secretWord);
+    let button=document.getElementById("numberGuesses").value;
+    let greeting=document.getElementById("greeting");
+    greeting.innerHTML="Fantastic word choice, " + playerOne + " You have given " + numberGuesses.value + " guesses.";
+    const formData= new FormData(form);
+    const queryString = new URLSearchParams(formData).toString();
+    setCookie(secretWord);
 
+    // window.location.href= "playerTwo.html?" + queryString;
+//gamePlay();
+}
+function setCookie(secretWord){
+    var date = new Date();
+    var expires = "; expires=" + date.toGMTString();
+    var cookieString = "secretWord" + "=" + secretWord + expires + ";path=/";
+    document.cookie = cookieString;
+    console.log(cookieString);
+    
+}
+function getCookie(secretWord) {
+    let word = secretWord + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+    if (c.indexOf(word) == 0) {
+        return c.substring(word.length, c.length);
+    }
+    }
+    return "";
+}
